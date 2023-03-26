e490cd0d7ba13d515b3f0709c8685ec95840b3ab
Aysha Hussain <ayshafajar3@gmail.com>
2023-03-26 20:59:47 +0000

    completed strings

---
+++ strings/formatted.py
@@ -0,0 +1,4 @@
+first_name = 'Aysha'
+last_name = 'Hussain'
+message = f'My name is {first_name} {last_name}'
+print(message)
---
+++ strings/methods.py
@@ -0,0 +1,7 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.find('f'), 'returns index')
+print('for' in course)
+print('For' in course)
+print(course.title())
